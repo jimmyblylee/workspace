4a6f068586db43d8ce2e938dfe9695e4e51ea93d
Jimmybly Lee <jimmyblylee@126.com>
2016-09-15 23:22:26 +0800

    [snapshot] there is no need to add tomcat server configuration of server.xml and context.xml and there is no need to add config_jdbc.bat to dev script

--- src/tomcat/src/main/workspace/win/dev/config_jdbc.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-title Registering the datasources by the Configuration of server.xml and context.xml
-set BASEDIR=%~dp0
-call %BASEDIR%\setEnv.bat >NUL
-
-xcopy /Y %WORKSPACE_FOLDER%\common\jdbc\* %TOMCAT_HOME%\conf\ >NUL 2>NUL
